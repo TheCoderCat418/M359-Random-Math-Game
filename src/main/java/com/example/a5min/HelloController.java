5436190a95aa874d20df93812ddf7454341c9457
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-09-04 11:49:11 -0500

    end of in-class 9/4/24.

--- src/main/java/com/example/a5min/HelloController.java
+++ src/main/java/com/example/a5min/HelloController.java
@@ -1,21 +1,75 @@
 package com.example.a5min;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label title;
-    @FXML
-    private TextField txtInput;
-    @FXML
-    private Button main;
+
+    public TextField name1;
+    public TextField name2;
+    public TextField rounds;
+    public Button d1;
+    public Button d2;
+    public Button d3;
+    public TextField range1;
+    public TextField range2;
+    public Button plus;
+    public Button minus;
+    public Button division;
+    public Button start;
+    public Button multiplication;
+    public TextField answer;
+
+    private boolean singlePlayer = true;
+    private int dificulty = 1;
+    private int operation = 1;
 
     @FXML
-    protected void mainOnClick() {
-        title.setText(txtInput.getText());
-        System.out.println(txtInput.getText());
+    protected void onGameStart(){
+        if(!name2.getText().isBlank()){
+            singlePlayer = false;
+        }
+
+
+    }
+    @FXML
+    protected void updateDificulty(ActionEvent ae){
+        Button pushed = (Button) ae.getSource();
+        if(pushed.equals(d1)){
+            dificulty = 1;
+            d1.setDisable(true);
+            d2.setDisable(false);
+            d3.setDisable(false);
+        }else if(pushed.equals(d2)) {
+            dificulty = 2;
+            d1.setDisable(false);
+            d2.setDisable(true);
+            d3.setDisable(false);
+        } else if(pushed.equals(d3)){
+            dificulty = 3;
+            d1.setDisable(false);
+            d2.setDisable(false);
+            d3.setDisable(true);
+        }
+    }
+    @FXML
+    private void updateOperation(ActionEvent ae){
+        Button pushed = (Button) ae.getSource();
+        if(pushed.equals(plus)){
+            operation = 1;
+            plus.setDisable(true);
+            minus.setDisable(false);
+        }else if(pushed.equals(minus)){
+            operation = 2;
+            plus.setDisable(false);
+            minus.setDisable(true);
+        }
+    }
+
+    private Question generateQuestion(int rangeMin, int rangeMax, int operation){
+        return null;
     }
 }
--- src/main/java/com/example/a5min/Question.java
+++ src/main/java/com/example/a5min/Question.java
@@ -2,6 +2,38 @@ package com.example.a5min;
 
 public class Question {
     private int operation;
-    private int[] randomNumbers;
-    private int difficultyLevel;
+    private String randomNumbers;
+    private int answer;
+
+    public Question(int operation, int randomMin, int randomMax){
+        this.operation = operation;
+        this.randomNumbers = assembleRandomNumbers(randomMin, randomMax);
+        this.answer = processAnswer();
+    }
+    private int processAnswer(){
+        String[] rns = randomNumbers.split(",");
+        int num1 = Integer.parseInt(rns[0]);
+        int num2 = Integer.parseInt(rns[1]);
+
+        if(operation == 1){
+            return num1 + num2;
+        }
+        throw new RuntimeException("Invalid Operation");
+    }
+    private String assembleRandomNumbers(int min, int max){
+        int number1 = min+(int)(Math.random() * ((max - min) + 1));
+        int number2 = min+(int)(Math.random() * ((max - min) + 1));
+
+        return number1+","+number2;
+    }
+
+    public int getOperation() {
+        return operation;
+    }
+    public int getAnswer(){
+        return answer;
+    }
+    public String getRandomNumbers(){
+        return randomNumbers;
+    }
 }
