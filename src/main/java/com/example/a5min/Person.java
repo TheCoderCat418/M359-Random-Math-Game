593daee498e6f6b7887a1aded46a1abb00331a11
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-09-03 22:13:02 -0500

    end of homework. 9/3/24

--- src/main/java/com/example/a5min/Person.java
+++ src/main/java/com/example/a5min/Person.java
@@ -1,4 +1,8 @@
 package com.example.a5min;
 
 public class Person {
+    private String name;
+    private int score;
+    private int levelOfAchievement;
+    private int highScore;
 }
---
+++ src/main/java/com/example/a5min/Question.java
@@ -0,0 +1,7 @@
+package com.example.a5min;
+
+public class Question {
+    private int operation;
+    private int[] randomNumbers;
+    private int difficultyLevel;
+}
